0c94706d7bcb99913d834a7d3d931f10ff4775a1
sankarmanoj <sankar-manoj@me.com>
2016-09-07 16:03:52 +0530

    Moved to Demo Folder

--- condition.py
+++
@@ -1,3 +0,0 @@
-for x in range(100):
-    if x%3==0:
-        print x
--- socketClient.py
+++
@@ -1,5 +0,0 @@
-from socket import *
-client = socket()
-client.connect(("localhost",5432))
-while True:
-    client.send(raw_input("Enter Input"))
